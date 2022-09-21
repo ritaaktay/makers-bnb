ad42a1126859dc439821f5ac264df84cfa3d080d
ritaaktay <ritaaktay@gmail.com>
2022-09-21 12:13:12 +0100

    database set up done

--- spec/seeds/listings_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE listings (
-  id SERIAL PRIMARY KEY,
-  price_per_night numeric,
-  availability date [],
-  space_id int,
-  constraint fk_space foreign key (space_id) 
-    references spaces(id) 
-    on delete cascade
-);
